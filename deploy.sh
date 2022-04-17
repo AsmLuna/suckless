3954c9feb02253e53bb2e4b8b91a5bbaeab08e7b
OpenSky <82097160+opensky0@users.noreply.github.com>
2022-04-17 13:29:30 +0300

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+sudo pacman -S --noconfirm --needed ttf-font-awesome dmenu nitrogen ttf-dejavu ttf-liberation gnu-free-fonts lxappearance
+
+git clone https://github.com/opensky0/suckless
+cd suckless
+make
+sudo make install
